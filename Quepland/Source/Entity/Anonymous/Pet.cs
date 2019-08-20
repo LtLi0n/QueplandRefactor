7dbcbcd7ccee616777c94001c25d67068386c7f4
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2019-08-21 01:00:46 +0300

    Moved Pet and Room types to Anonymous folder. Still considering naming string Id entities something else, probably Snowflake.

---
+++ Quepland/Source/Entity/Anonymous/Pet.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace Quepland
+{
+    public class Pet : IEntity<Pet, string>
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public int Cost { get; }
+        public int MinLevel { get; }
+        public SkillType AffinitySkillType { get; }
+        public bool CanBuy { get; }
+
+        public string Nickname { get; set; }
+        public List<Skill> Skills { get; }
+
+        public Pet(
+            string id, 
+            string name, 
+            string description, 
+            int cost, 
+            int minLevel, 
+            SkillType affinity,
+            bool canBuy)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Cost = cost;
+            MinLevel = minLevel;
+            AffinitySkillType = affinity;
+            CanBuy = canBuy;
+            Nickname = string.Empty;
+            Skills = new List<Skill>();
+        }
+
+        public static Pet Chicken { get; } = new Pet(
+            id: "chicken",
+            name: "Chicken",
+            description: 
+            "The Chicken is a loyal companion, or she would be if her brain weren't the size of a pea." +
+            " She'll follow you around and help you grow in your skills a little bit more quickly." +
+            " She has a natural affinity for Culinary Arts, on account of tasting so good.",
+            cost: 15000,
+            minLevel: 1,
+            affinity: SkillType.Culinary_Arts,
+            canBuy: false);
+
+        public static class Buyable
+        {
+            public static Pet Scarab { get; } = new Pet(
+                id: "scarab",
+                name: "Scarab",
+                description:
+                "The scarab is a shiny companion." +
+                " His hard outer shell makes it hard to be friends with him, but he'll help you grow in your skills a little more quickly." +
+                " He has a natural affinity for Woodworking, as it's his favorite food.",
+                cost: 10,
+                minLevel: 1,
+                affinity: SkillType.Woodworking,
+                canBuy: true);
+        }
+    }
+}
---
+++ Quepland/Source/Entity/Anonymous/Room.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace Quepland
+{
+    public class Room : IEntity<Room, string>
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public int ConstructionLevelRequired { get; }
+        public int MinimumPlankLevel { get; }
+        public int PlanksRequired { get; }
+        public int MinimumBarLevel { get; }
+        public int BarsRequired { get; }
+
+        public int[][] OtherItemCosts { get; set; }
+        public int ExperienceGained { get; set; }
+        public bool IsComplete { get; set; }
+        public int WorkRemaining { get; set; }
+        public List<Furniture> Furniture { get; } = new List<Furniture>();
+
+        public Room(
+            string id, 
+            string name, 
+            string description, 
+            int constructionLevelRequired, 
+            int minimumPlankLevel, 
+            int planksRequired, 
+            int minimumBarLevel, 
+            int barsRequired)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ConstructionLevelRequired = constructionLevelRequired;
+            MinimumPlankLevel = minimumPlankLevel;
+            PlanksRequired = planksRequired;
+            MinimumBarLevel = minimumBarLevel;
+            BarsRequired = barsRequired;
+        }
+
+        public static Room Master_Bedroom { get; } = new Room(
+            id: "Master_Bedroom",
+            name: "Master Bedroom",
+            description: "Your main bedroom. You can build various pieces of furniture to make it more comfortabal. Your house can only have one master bedroom.",
+            constructionLevelRequired: 1,
+            minimumPlankLevel: 0,
+            planksRequired: 1000,
+            minimumBarLevel: 0,
+            barsRequired: 500);
+
+        public static Room Arena { get; } = new Room(
+            id: "Arena",
+            name: "Arena",
+            description: "The arena is a place in your home that lets you kill things. Simply build a kyonshi of the monster you want to kill, and activate it for private farming.",
+            constructionLevelRequired: 20,
+            minimumPlankLevel: 1,
+            planksRequired: 3000,
+            minimumBarLevel: 2,
+            barsRequired: 1500);
+
+        public static Room Chicken_Room { get; } = new Room(
+            id: "Chicken_Room",
+            name: "Chicken Room",
+            description: "The chicken room can be filled with chicken coops, which produce eggs and feathers daily.",
+            constructionLevelRequired: 10,
+            minimumPlankLevel: 2,
+            planksRequired: 4000,
+            minimumBarLevel: 0,
+            barsRequired: 250);
+
+    }
+}
