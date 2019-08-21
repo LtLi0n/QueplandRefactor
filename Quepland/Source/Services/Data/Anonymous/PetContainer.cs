31803d1424a140ee609f1b5e1925a3e9fcabbadd
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2019-08-21 14:30:23 +0300

    Remade current containers to support previous commit. I must admit, This looks a lot cleaner.

---
+++ Quepland/Source/Services/Data/Anonymous/PetContainer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Quepland
+{
+    public class PetContainer : StaticEntityContainerBase<Pet, string>
+    {
+        public PetContainer() { }
+
+        protected override IEnumerable<Pet> LoadContent() =>
+            new Pet[]
+            {
+                Pet.Chicken,
+                Pet.Buyable.Scarab
+            };
+
+        public Pet GetByName(string name) => _content.Values.FirstOrDefault(x => x.Name == name);
+    }
+}
---
+++ Quepland/Source/Services/Data/Anonymous/RoomContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Quepland
+{
+    public class RoomContainer : StaticEntityContainerBase<Room, string>
+    {
+        public RoomContainer() { }
+
+        protected override IEnumerable<Room> LoadContent() =>
+            new Room[] 
+            { 
+                Room.Master_Bedroom, 
+                Room.Arena, 
+                Room.Chicken_Room 
+            };
+
+        public Room GetByName(string name) => _content.Values.FirstOrDefault(x => x.Name == name);
+    }
+}
